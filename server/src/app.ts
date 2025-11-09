6c2abbfd798105c6bcca1e44794406b7f39dcf31
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-11-09 20:54:08 +0530

    chore(client, server): add vercel.json to client and enable trust proxy on server

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,6 +20,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set("trust proxy", true);
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/user", userRoute);
